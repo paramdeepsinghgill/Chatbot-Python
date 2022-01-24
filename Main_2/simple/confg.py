232eda09e4c03e50041b4c5cee4d9f2f91f0a5a3
paramdeepsinghgill <43493969+paramdeepsinghgill@users.noreply.github.com>
2022-01-24 12:51:49 +0530

    Update confg.py

--- Main_2/simple/confg.py
+++ Main_2/simple/confg.py
@@ -1,8 +1,8 @@
-password = "p"
-my_email = "psgillbot@gmail.com"
-email_password = "param@1234"
-gmail_server = 'smtp.gmail.com'
-consumer_key = "sowy6hCPWcYmabLlJlDf4FzFr"
-consumer_secret = "WgpVYsX8FoKajamHkkxnFcLnJjlJgpJ1u8GZcubPTJbcoTLhnH"
-access_token = "1089739186373095424-74hb6OdXOP5hb2qNZm3YwAxbJfDxnf"
-access_token_secret = "L7IURXT7F82ILfP9JC7PnMtPproeCllYaw9WBwjxkMc0N"
+password = 
+my_email = 
+email_password = 
+gmail_server = 
+consumer_key = 
+consumer_secret = 
+access_token = 
+access_token_secret = 
